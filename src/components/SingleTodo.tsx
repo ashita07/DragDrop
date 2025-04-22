86a0997d4aa22bb9d62db974dd22a0a41b06d327
Ashita <ashitam2407@gmail.com>
2025-04-22 19:23:26 +0530

    finished the tasklist section

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { JSX } from "react";
+import React, { JSX, useEffect, useRef, useState } from "react";
 import { Todo } from "../model";
 import Tick from "../icons/Tick";
 import Delete from "../icons/Delete";
@@ -12,18 +12,65 @@ interface Props {
 }
 
 const SingleTodo: React.FC<Props> = ({ t, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string | number>(t.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   return (
-    <form className="todos__single">
-      <span className="todos__single--text">{t.todo}</span>
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, t.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos__single--text"
+        />
+      ) : t.isDone ? (
+        <s className="todos__single--text">{t.todo}</s>
+      ) : (
+        <span className="todos__single--text">{t.todo}</span>
+      )}
+
       <div className="inlineIcons">
-        <span className="icon">
+        <span
+          onClick={() => {
+            if (!edit && !t.isDone) {
+              setEdit(!edit);
+            }
+          }}
+          className="icon"
+        >
+          <Edit />
+        </span>
+        <span onClick={() => handleDone(t.id)} className="icon">
           <Tick />
         </span>
-        <span className="icon">
+        <span onClick={() => handleDelete(t.id)} className="icon">
           <Delete />
-        </span>
-        <span className="icon">
-          <Edit />
         </span>
       </div>
     </form>
